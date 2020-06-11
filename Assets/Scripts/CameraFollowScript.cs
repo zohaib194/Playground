8285427925eabb3c85e3147f0578be1698aa8c3e
Zohaib Butt <zohaib_b13@hotmail.com>
2020-06-11 21:24:35 +0200

    3rd camera script is working

--- Assets/Scripts/CameraFollowScript.cs
+++ Assets/Scripts/CameraFollowScript.cs
@@ -2,21 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollowScript : MonoBehaviour
-{
-	public GameObject cam;
-
+public class CameraFollowScript : MonoBehaviour {
+	public GameObject target;
+    public bool followPlayer = false;
+    public float walkDistance = 10.0f;
+    public float runDistance = 7.0f;
+    public float height = 5.0f;
+    public float rotationDamping = 5.0f;
+    public float heightDamping = 5.0f;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        cam.transform.position = new Vector3(this.gameObject.transform.position.x, 
-        									this.gameObject.transform.position.y + 10.0f, 
-        									this.gameObject.transform.position.z - 10.0f);
+    void Update() {   
+
+    }
+
+    void LateUpdate() {
+        
+        float wantedRotationAngle = target.transform.eulerAngles.y;
+        float wantedHeight = target.transform.position.y + height;
+
+        float currentRotationAngle = this.gameObject.transform.eulerAngles.y;
+        float currentHeight = this.gameObject.transform.position.y;
+
+        // Damp the rotation around y-axis
+        currentRotationAngle = Mathf.LerpAngle(
+            currentRotationAngle, 
+            wantedRotationAngle, 
+            rotationDamping * Time.deltaTime
+        );
+
+        currentHeight = Mathf.Lerp(
+            currentHeight, 
+            wantedHeight, 
+            heightDamping * Time.deltaTime
+        );  
+
+        // Convert angles into rotation
+        Quaternion currentRotation = Quaternion.Euler(0.0f, currentRotationAngle, 0.0f);
+
+        this.gameObject.transform.position = target.transform.position;
+        this.gameObject.transform.position -= currentRotation * Vector3.forward * walkDistance;
+
+        this.gameObject.transform.position = new Vector3(
+            this.gameObject.transform.position.x, 
+            currentHeight,
+            this.gameObject.transform.position.z
+        );
+
+        this.gameObject.transform.LookAt(target.transform);
+
+
+        /*this.gameObject.transform.position = new Vector3(
+            target.transform.position.x,
+            target.transform.position.y + height,
+            target.transform.position.z - walkDistance
+        );
+
+        this.gameObject.transform.LookAt(target.transform); */
     }
 }
--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -23,13 +23,14 @@ public class MovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.z, 10);
-        Vector3 lookPos = Camera.main.ScreenToWorldPoint(mousePos);
-        lookPos = lookPos - transform.position;
-        float angle = Mathf.Atan2(lookPos.z, lookPos.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.down); // Turns Right
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.up); //Turns Left
-
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 characterPosition = Camera.main.WorldToScreenPoint(this.gameObject.transform.position);
+        mousePos.x = characterPosition.x - mousePos.x;
+        mousePos.y = characterPosition.y - mousePos.y;
+ 
+        float angle = Mathf.Atan2(mousePos.x, mousePos.y) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, angle, 0));
+       
     	if(characterController.isGrounded) {
 
 
